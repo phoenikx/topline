5bdc08b99ff706e42405033a2779e5e646019cbc
Nikhil Ranjan <nikhil.ranjan@harness.io>
2021-06-07 12:11:03 +0530

    Initial commit

---
+++ src/main/java/io/harness/topline/ToplineApplication.java
@@ -0,0 +1,13 @@
+package io.harness.topline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToplineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToplineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/harness/topline/ToplineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.harness.topline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToplineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
