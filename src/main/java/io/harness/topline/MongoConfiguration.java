f5afecf13224eaafb4673f265a1be4908df192e9
Nikhil Ranjan <nikhil.ranjan@harness.io>
2021-06-08 10:17:54 +0530

    Add update endpoint

---
+++ src/main/java/io/harness/topline/MongoConfiguration.java
@@ -0,0 +1,37 @@
+package io.harness.topline;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Configuration
+public class MongoConfiguration extends AbstractMongoClientConfiguration {
+    @Value("${mongo.uri}") private String uri;
+
+    @Override
+    protected String getDatabaseName() {
+        return "topline";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(uri);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Override
+    public Collection getMappingBasePackages() {
+        return Collections.singleton("io.harness.topline");
+    }
+}
